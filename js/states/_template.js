e0bb76b69a5adaa6b287e9b59b91f707e7d57553
Dave Nicholson <fatnic388@gmail.com>
2016-06-09 18:30:54 +0100

    created a template for future states

---
+++ js/states/_template.js
@@ -0,0 +1,12 @@
+function state_Template() {
+
+    this.update = function() {
+        //
+    };
+
+    this.draw = function() {
+        DrawTools.clear(game.ctx, 'rgb(51,51,51)');
+        //
+    };
+
+}
